37e3f360c399df826246faa73c3881ea1289a630
uapv2401806 <uapv2401806@e-mc136-24.ad.univ-avignon.fr>
2025-05-12 10:20:49 +0200

    🔁 Mises à jour des fichiers compilés : compilateur, test, test.s

--- test.s
+++ test.s
@@ -1,71 +1,39 @@
-			# This code was produced by the CERI Compiler
+			# Code généré automatiquement par MonCompilateur
+	.extern printf
 	.data
 	.align 8
 a:	.quad 0
 b:	.quad 0
-c:	.quad 0
-z:	.quad 0
-	.text		# The following lines contain the program
-	.globl main	# The main function must be visible from outside
-main:			# The main function body :
-	movq %rsp, %rbp	# Save the position of the stack's top
-	push $8
-	push $3
-	pop %rax
-	pop %rbx
-	cmpq %rax, %rbx
-	je Vrai1	# If equal
-	push $0		# False
-	jmp Suite1
-Vrai1:	push $0xFFFFFFFFFFFFFFFF		# True
-Suite1:
+	.text
+	.globl main
+main:
+	movq %rsp, %rbp
 	push $4
-	push $2
+	push $3
+	pop %rbx
+	pop %rax
+	addq	%rbx, %rax	# ADD
+	push %rax
+	pop a
+	push a
 	push $2
 	pop %rbx
 	pop %rax
 	mulq	%rbx
-	push %rax
-	pop %rax
-	pop %rbx
-	cmpq %rax, %rbx
-	je Vrai2	# If equal
-	push $0		# False
-	jmp Suite2
-Vrai2:	push $0xFFFFFFFFFFFFFFFF		# True
-Suite2:
-	pop %rbx
-	pop %rax
-	subq	%rbx, %rax
-	push %rax
-	pop z
-	push $5
-	push $65
-	pop %rbx
-	pop %rax
-	movq $0, %rdx
-	div %rbx
-	push %rax
-	push $2
-	pop %rbx
-	pop %rax
-	addq	%rbx, %rax
-	push %rax
-	push $7
-	push $5
-	pop %rbx
-	pop %rax
-	movq $0, %rdx
-	div %rbx
-	push %rdx
-	pop %rax
-	pop %rbx
-	cmpq %rax, %rbx
-	jb Vrai3	# If below
-	push $0		# False
-	jmp Suite3
-Vrai3:	push $0xFFFFFFFFFFFFFFFF		# True
-Suite3:
+	push %rax	# MUL
 	pop b
-	movq %rbp, %rsp		# Restore the position of the stack's top
-	ret			# Return from main function
+	movq a, %rsi
+	leaq msg_a(%rip), %rdi
+	xor %rax, %rax
+	call printf
+	movq b, %rsi
+	leaq msg_b(%rip), %rdi
+	xor %rax, %rax
+	call printf
+	movq %rbp, %rsp
+	ret
+	.section .rodata
+msg_a:	.string "Valeur de a : %ld\n"
+msg_b:	.string "Valeur de b : %ld\n"
+	.section .note.GNU-stack,"",@progbits
+	.section .note.GNU-stack,"",@progbits
