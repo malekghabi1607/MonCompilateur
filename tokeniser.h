c401915e2d38f1c0a63f42b138f039aca2be5a83
uapv2401806 <uapv2401806@e-mc131-20.ad.univ-avignon.fr>
2025-05-14 09:52:32 +0200

    Ajout de l'énumération MOTCLEVAL dans tokeniser.h (TP3)

--- tokeniser.h
+++ tokeniser.h
@@ -1,4 +1,20 @@
-// tokeniser.h : shared definition for tokeniser.l and compilateur.cpp
+// tokeniser.h
 
-enum TOKEN {FEOF, UNKNOWN, NUMBER, ID, STRINGCONST, RBRACKET, LBRACKET, RPARENT, LPARENT, COMMA, 
-SEMICOLON, DOT, ADDOP, MULOP, RELOP, NOT, ASSIGN};
+#ifndef TOKENISER_H
+#define TOKENISER_H
+
+enum TOKEN {
+    FEOF, UNKNOWN, NUMBER, ID, STRINGCONST, RBRACKET, LBRACKET,
+    RPARENT, LPARENT, COMMA, SEMICOLON, DOT, ADDOP, MULOP,
+    RELOP, NOT, ASSIGN, MOTCLE 
+};
+
+// Juste la **déclaration** de l'enum des mots-clés
+enum MOTCLEVAL {
+    IF_, THEN_, ELSE_, WHILE_, DO_, FOR_, TO_, BEGIN_, END_, UNKNOWN_KEYWORD
+};
+
+// Juste la **déclaration** de la fonction :
+int GetKeyword();
+
+#endif
