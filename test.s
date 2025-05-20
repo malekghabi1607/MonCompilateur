0fdda70401fb694e7985b0d0a07dde6d6056e884
uapv2401806 <uapv2401806@e-mc130-01.ad.univ-avignon.fr>
2025-05-20 17:32:06 +0200

    Ajout du TP7 : gestion des types DOUBLE et CHAR - Implémentation des constantes flottantes et caractères - Mise à jour de l'analyse lexicale pour reconnaître DOUBLE_CONST et CHARCONST - Adaptation des opérations arithmétiques pour DOUBLE avec la pile x87 - Gestion des allocations mémoire pour les nouveaux types - Modification de l'instruction DISPLAY pour afficher correctement DOUBLE et CHAR

--- test.s
+++ test.s
@@ -1,50 +1,16 @@
 			# Code généré automatiquement par MonCompilateur
 	.extern printf
-a:	.quad 0
-b:	.quad 0
-c:	.quad 0
+DEBUG VarDeclarationPart: current token = x
+x:	.double 0.0
+y:	.double 0.0
+DEBUG VarDeclarationPart loop: current token = c
+c:	.byte 0
+DEBUG VarDeclarationPart end: current token = BEGIN
 	.text
 	.globl main
 main:
 	movq %rsp, %rbp
-DEBUG BlockStatement : current token = 3, texte = 'a'
-	push $10
-	pop a
-	push $20
-	pop b
-	movq a(%rip), %rax
-	push %rax
-	movq b(%rip), %rax
-	push %rax
-	pop %rax
-	pop %rbx
-	cmpq %rax, %rbx
-	jb Vrai1
-	push $0
-	jmp Suite1
-Vrai1:	push $-1
-Suite1:
-	pop c
-	movq a(%rip), %rax
-	push %rax
-	pop %rsi	# valeur à afficher
-	movq $FormatString1, %rdi	# format %llu\n
-	movl $0, %eax			# nombre d'arguments flottants
-	call printf@PLT
-	movq b(%rip), %rax
-	push %rax
-	pop %rsi	# valeur à afficher
-	movq $FormatString1, %rdi	# format %llu\n
-	movl $0, %eax			# nombre d'arguments flottants
-	call printf@PLT
-	movq c(%rip), %rax
-	push %rax
-	pop %rsi	# valeur booléenne à afficher
-	cmpq $0, %rsi
-	je BoolFalse4
-	movq $TrueString, %rdi	# chaîne TRUE
-	jmp BoolEnd4
-BoolFalse4:
-	movq $FalseString, %rdi	# chaîne FALSE
-BoolEnd4:
-	call puts@PLT
+DEBUG BlockStatement : current token = 3, texte = 'x'
+	push $3
+	pop x
+DEBUG avant END check : current token = 11, texte = '.'
